9d01936817f590be7fa4a905d5ceff3a808abd86
nligon <nathanaelligon@gmail.com>
2016-05-11 19:13:22 -0700

    Initial commit.

--- web/basic-server.js
+++ web/basic-server.js
@@ -1,5 +1,5 @@
 var http = require('http');
-var handler = require('./request-handler');
+var handler = require('./request-handler.js');
 var initialize = require('./initialize.js');
 
 // Why do you think we have this here?
@@ -11,9 +11,9 @@ var ip = '127.0.0.1';
 var server = http.createServer(handler.handleRequest);
 
 if (module.parent) {
+  console.log("encountered 'module.parent'");
   module.exports = server;
 } else {
   server.listen(port, ip);
   console.log('Listening on http://' + ip + ':' + port);
 }
-
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,68 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+var url = require('url');
 // require more modules/folders here!
 
-exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+var helpers = require('./http-helpers.js');
+
+var results = [];
+
+
+exports.handleRequest = function(request, response) {
+  console.log("request method: ", request.method);
+  var statusCode = 200;
+
+  // .writeHead() writes to the request line and headers of the response,
+  // which includes the status and all headers.
+
+  if (request.method === 'GET') {
+
+    // request.on('data', function(chunk) {
+    //   console.log('Parsed: ', JSON.parse(chunk));
+    //   console.log('Stringed', chunk.toString());
+    //   resultsObj.results.push(JSON.parse(chunk));
+    //   console.log('results: ', resultsObj);
+    //   console.log('Stringified: ', JSON.stringify(results[0]));
+    // });
+
+    // var url_parts = url.parse(request.url);
+    // console.log("parts", url_parts);
+    // console.log("parts and pathname", url_parts.pathname);
+
+    statusCode = 200;
+    var filePath = archive.paths.archiveSites + request.url;
+    console.log("request URL: ", filePath);
+
+    fs.readFile(filePath, 'utf8', function(err, contents) {
+      results = contents;
+      response.writeHead(statusCode, helpers.headers);
+      response.end(JSON.parse(contents));
+    });
+
+  } else if (request.method === 'POST') {
+    statusCode = 201;
+    request.on('data', function(data) {
+      var filePath = archive.paths.list;
+      fs.writeFile(filePath, data, function(err) {
+        if (err) {throw err};
+        response.writeHead(statusCode, helpers.headers);
+        response.end();
+      });
+    });
+
+
+  }
+
+  if (request.url !== '/classes/messages' && request.url !== '/') {
+    statusCode = 404;
+
+  }
+
+  // response.writeHead(statusCode, helpers.headers);
+
+  // // response.end(/Volumes/student/Web-historian/test/testdata/sites.txt)
+  // // response.end('archive.paths.list');
+  // console.log('results: ' + results);
+  // response.end();
 };
